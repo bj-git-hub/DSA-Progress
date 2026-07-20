fff5d17e182a3a6c3ffad79fa2633de72a0ba622
bj-git-hub <bj349111@gmail.com>
2026-07-20 19:15:17 +0530

    beginning 2

--- Striver DSA/C++ STL/pair.cpp
+++ Striver DSA/C++ STL/pair.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main(){
+    // initialisation
     pair <int, int> p = {1,2};
     cout << p.first << " " << p.second;
+
+    // nested pair
+    pair <int , pair <int, int>>p2 = {1,{2,3}};
+    cout << p2.first << " " << p2.second.first << " " << p2.second.second;
+
+    pair <pair <int, int> , pair<int, int>> p3 = {{1,2}, {3,4}};
+    
+    cout << p3.first.first << " " << p3.first.second << " " 
+    << p3.second.first << " " << p3.second.second;
+    // and so on....
+
+    // pair can also be array's data type
+    pair<int, int> arr[] = {{1,2},{3,4},{4,5}};
+    cout << arr[0].first;
 }
---
+++ Striver DSA/C++ STL/vector.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#define MAX 10;
+
+int main(){
+    cout << MAX;
+}
