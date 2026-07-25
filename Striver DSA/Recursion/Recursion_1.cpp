6dd94f569bdcd406f450cca937ac43f9467d0b16
bj-git-hub <joshi.bhavesh.in@gmail.com>
2026-07-25 13:44:35 +0530

    Recursion Basics Complete

--- Striver DSA/Recursion/Recursion_1.cpp
+++ Striver DSA/Recursion/Recursion_1.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+// Recursion is when a function calls itself, until a specified condition is met.
+// The specified condition is also known as base condition.
+
+void printName(int i, int n){
+    if(i > n) return;
+    cout << "Bhavesh" << endl;
+    printName(i+1, n);
+}
+
+void print1toN(int i, int n){
+    if(i > n) return;
+    cout << i << " ";
+    print1toN(i+1, n);
+}
+
+void printNto1(int i, int n){
+    if(i > n ) return;
+    cout << n << " ";
+    printNto1(1, n-1);
+}
+
 int main(){
-    return 0;
+    int n;
+    cin >> n;
+    printName(1, n);
+    print1toN(1,n);
+    cout << endl;
+    printNto1(1, n);
+
+    
 }
