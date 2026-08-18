a7db45c17d9840c41535e168ebdfbbed48285dab
bj-git-hub <joshi.bhavesh.in@gmail.com>
2026-08-19 00:11:54 +0530

    Merge Sort Complete

---
+++ Striver DSA/Sorting Techniques/Sorting-II/mergeSort.cpp
@@ -0,0 +1,73 @@
+/*
+Merge Sort works on the principle of divide and conquer.
+We divide the dataset recursively, until it just has one index each division.
+then we combine, and merge the single element array, as we move up the recursion, giving us the final sorted
+array.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector <int> &v, int low, int mid, int high);
+void mergeSort(vector <int> &v, int low, int high);
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> v;
+    for(int i = 0; i < n; i ++){
+        int num;
+        cin >> num;
+        v.emplace_back(num);
+    }
+    mergeSort(v, 0, n - 1);
+
+    for(int vi: v){
+        cout << vi << " ";
+    }
+}
+void mergeSort(vector <int> &v, int low, int high){
+
+    if (low == high) return; // single element present in array.
+    int mid = (low + high)/2;
+    mergeSort(v, low, mid ); // calling for left half
+    mergeSort(v, mid + 1, high); // calling for right half
+    merge(v, low, mid, high);
+}
+
+void merge(vector <int> &v, int low, int mid, int high){
+    int left = low;
+    int right = mid + 1;
+    vector <int> temp;
+    while(left<= mid && right <= high){
+        if(v[left] <= v[right]) {  // main condition
+            temp.emplace_back(v[left]);
+            left++;
+        }
+        else{
+            temp.emplace_back(v[right]);
+            right++;
+        }
+    }
+    while(left <= mid){
+        temp.emplace_back(v[left]);
+        left++;
+    }
+    while(right <= high){
+        temp.emplace_back(v[right]);
+        right++;
+    }
+    for(int i = low; i <= high; i++){
+        v[i] = temp[i - low ];
+    }
+}
+/*
+Coding Approach:
+In order to code;
+Base case must be clear => single element remaining i.e. low == high
+two recursive calls inside mergeSort function.
+and one merge function call inside the mergeSort function.
+
+*/ 
