8335e7255a1f2c0efce26d854940117abdb31255
bj-git-hub <joshi.bhavesh.in@gmail.com>
2026-08-20 18:25:41 +0530

    Revise Merge Sort

--- Striver DSA/Sorting Techniques/Sorting-II/mergeSort.cpp
+++ Striver DSA/Sorting Techniques/Sorting-II/mergeSort.cpp
@@ -51,16 +51,17 @@ void merge(vector <int> &v, int low, int mid, int high){
             right++;
         }
     }
-    while(left <= mid){
+    while(left <= mid){ // if right array gets exhausted.
         temp.emplace_back(v[left]);
         left++;
     }
-    while(right <= high){
+    while(right <= high){ // if left array gets exhausted.
         temp.emplace_back(v[right]);
         right++;
     }
     for(int i = low; i <= high; i++){
-        v[i] = temp[i - low ];
+        v[i] = temp[i - low ]; // can't just be i because the value of low changes at every recursive call
+                               // so we need it to be in relation with low.
     }
 }
 /*
